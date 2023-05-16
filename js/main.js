28bac5a9c01ed8269220dc80001b1eb8ddfe58d8
Menta <simcictilen.gmail.com>
2023-05-16 15:51:42 +0200

    creati i 5 post su html attraverso js

--- js/main.js
+++ js/main.js
@@ -75,3 +75,46 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let elSelector = document.getElementById("container");
+
+posts.forEach((element, index) => {
+    elSelector.innerHTML += `
+                            <div class="post">
+                                <div class="post__header">
+                                    <div class="post-meta">                    
+                                        <div class="post-meta__icon">
+                                            <img class="profile-pic" src="${posts[index].author.image}" alt="${posts[index].author.name}">                    
+                                        </div>
+                                        <div class="post-meta__data">
+                                            <div class="post-meta__author">
+                                                ${posts[index].author.name}
+                                            </div>
+                                            <div class="post-meta__time">
+                                                ${posts[index].created}
+                                            </div>
+                                        </div>                    
+                                    </div>
+                                </div>
+                                <div class="post__text">
+                                    ${posts[index].content}
+                                </div>
+                                <div class="post__image">
+                                    <img src="${posts[index].media}" alt="">
+                                </div>
+                                <div class="post__footer">
+                                    <div class="likes js-likes">
+                                        <div class="likes__cta">
+                                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                <span class="like-button__label">Mi Piace</span>
+                                            </a>
+                                        </div>
+                                        <div class="likes__counter">
+                                            Piace a <b id="like-counter-1" class="js-likes-counter">${posts[index].likes}</b> persone
+                                        </div>
+                                    </div> 
+                                </div>            
+                            </div>
+    `
+});
